42f969afcbec1c9dc5288fcd35e532855261a870
Charles <charlesthiagosantos@gmail.com>
2023-11-27 19:29:02 -0300

    feat : override na classe corrente

--- src/com/bancotrue/modelos/ContaCorrente.java
+++ src/com/bancotrue/modelos/ContaCorrente.java
@@ -8,4 +8,20 @@ public class ContaCorrente extends Conta {
         super(id, nome, saldo, imposto);
     }
 
+    @Override
+    public boolean depositar(double saldo) {
+        System.out.printf("Valor creditado na sua conta Corrente de %.2f\n", this.getSaldo());
+        return super.depositar(saldo);
+    }
+
+    @Override
+    public boolean sacar(double saldo) {
+        System.out.printf("Saque efetuado %s!", this.getNome());
+        return super.sacar(saldo);
+    }
+
+    @Override
+    public void tranferir(double saldo, Conta conta) {
+        super.tranferir(saldo-this.getImposto(), conta);
+    }
 }
